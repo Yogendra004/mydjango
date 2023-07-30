359dc6c88605f9f7bdca286162a664c51942839e
Yogi Singh <100386465+Yogendra004@users.noreply.github.com>
2023-07-30 23:25:03 +0100

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def starting_page(request):
+    return render (request,"blog/index.html")
+
+
+def posts (request):
+    pass
+
+def single_detail(request):
+    pass
